79fa82ff5d2e2e94fb8f389c8c8b965570271321
kata0049 <kata0049@algonquinlive.com>
2023-09-28 21:15:54 -0400

    Remove comments

--- lib/exercise2.dart
+++ lib/exercise2.dart
@@ -1,13 +1,3 @@
-/// import dart:io and dart:math packages
-
-/// In your lib folder there will be a dart file that has a
-/// class called Lucky.
-/// The Lucky class has a constructor which will create an instance of the class Lucky.
-/// The constructor will also
-/// read the two environment variables MIN and MAX
-/// create a random integer between those two numbers.
-/// Save the random int as a member field that can be accessed externally.
-
 import 'dart:math';
 import 'dart:io';
 
